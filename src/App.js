7c1dc862975901b5400e9fe8e4e8187128afd267
Kyungbong Ko <kyungbong135@gmail.com>
2023-03-10 23:20:13 -0800

    Finished website with minor visual bug

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [catPicUrl, setcatPicUrl] = useState('');
+  const [dogPicUrl, setdogPicUrl] = useState('');
+
+  const [showCatComment, setshowCatComment] = useState(false);
+  const [showDogComment, setshowDogComment] = useState(false);
+
+  const getCatPic = () => {
+    console.log('getCatPic called');
+    fetch("https://cataas.com/cat?json=true")
+    .then((response) => response.json())
+    .then((data) => {
+      setTimeout(() => {}, 1000);
+      setcatPicUrl("https://cataas.com" + data["url"]);
+      setshowCatComment(true);
+    });
+  };
+
+  const getDogPic = () => {
+    console.log('getDogPic called');
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      setTimeout(() => {}, 1000);
+      setdogPicUrl(data["message"]);
+      setshowDogComment(true);
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Which do you prefer?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="bothColumns">
+        <div className="catColumn"> 
+          <button className="catButton" onClick={getCatPic}>Cats</button>
+            {showCatComment && <p>
+              Good choice. Here's a picture of a cat.
+            </p>}
+            <div>
+              <img src={catPicUrl} className="Picture"/>
+            </div>
+          </div>
+        <div className="dogColumn"> 
+          <button className="dogButton" onClick={getDogPic}>Dogs???</button>
+            {showDogComment && <p>
+              Yay!!! Here's a doggo pic!!!
+            </p>}
+            <div>
+              <img src={dogPicUrl} className="Picture"/>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
